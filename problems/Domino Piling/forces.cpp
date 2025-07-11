0fea1aa7dd2258e64a67ef5b12f6da9a7acad5a8
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-07-11 19:52:41 +0300

    Domino Piling

---
+++ problems/Domino Piling/forces.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int M, N; cin >> M >> N;
+
+    cout << floor(M * N / 2) << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
