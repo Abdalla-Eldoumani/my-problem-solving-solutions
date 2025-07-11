2fa83196cf424434805706f5585bb93887230a09
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-07-11 20:16:37 +0300

    Elephant

---
+++ problems/Elephant/forces.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline void solve() {
+    int x; cin >> x;
+    cout << (x + 4) / 5 << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
