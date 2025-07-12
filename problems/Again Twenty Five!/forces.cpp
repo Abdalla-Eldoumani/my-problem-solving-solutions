72cb045a97b1014fa9a461c9697f217188294a49
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-07-12 23:27:54 +0300

    Again Twenty Five!

---
+++ problems/Again Twenty Five!/forces.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline void solve() {
+    long long n; cin >> n;
+
+    cout << "25\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
