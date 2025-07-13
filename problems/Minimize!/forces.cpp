012ad39f232086a5e49320b0a3aef779c2541194
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-07-13 16:57:06 +0300

    Minimize!

---
+++ problems/Minimize!/forces.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline void solve() {
+    int t; cin >> t;
+    while (t--) {
+        int a, b; cin >> a >> b;
+        cout << b - a << '\n';
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
