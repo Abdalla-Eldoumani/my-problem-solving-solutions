3c9db5bc8667fc8cd9dc367620046ba10bf7ac96
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-07-13 18:17:12 +0300

    Mahmoud and Ehab and the even-odd game

---
+++ problems/Mahmoud and Ehab and the even-odd game/forces.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline void solve() {
+    int n; cin >> n;
+    cout << (n % 2 == 0 ? "Mahmoud\n" : "Ehab\n");
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
