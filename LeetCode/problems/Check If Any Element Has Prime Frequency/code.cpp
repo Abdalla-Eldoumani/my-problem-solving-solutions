38b4f642a13636abdf66dfd77e3ee36044d3b167
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-08 18:18:02 -0600

    Check If Any Element Has Prime Frequency

---
+++ LeetCode/problems/Check If Any Element Has Prime Frequency/code.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    bool checkPrimeFrequency(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); ++i) {
+            mp[nums[i]]++;
+        }
+
+        for (int i = 0; i < mp.size(); ++i) {
+            if (isPrime(mp[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
