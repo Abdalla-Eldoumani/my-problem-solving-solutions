09702f4edb2050358415be1a38de883b87000e57
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-11 19:05:00 -0600

    Trippi Troppi

---
+++ CodeForces/problems/Trippi Troppi/forces.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline void solve() {
+    int t; cin >> t;
+    while (t--) {
+        string s1, s2, s3; cin >> s1 >> s2 >> s3;
+        cout << s1[0] << s2[0] << s3[0] << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
