caab95f8634b8dd75f22f778a1cbaf16992bd2bd
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-09-06 12:17:20 -0600

    EhAb AnD gCd

---
+++ CodeForces/problems/EhAb AnD gCd/forces.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline void solve() {
+    int t; cin >> t;
+    while (t--) {
+        int x; cin >> x;
+        cout << 1 << ' ' << x - 1 << '\n';
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
