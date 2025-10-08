3c0637387f2c541448a1e922c5d681ac5733e8dc
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-10-08 14:29:34 -0600

    Tweaked the code a little bit

--- LeetCode/problems/Perfect Number/code.cpp
+++ LeetCode/problems/Perfect Number/code.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        bool isPerfect = false;
+        if (num == 1) {
+            return false;
+        }
 
         int sum = 1;
         for (int i = 2; i <= sqrt(num); i++) {
             if (num % i == 0) {
                 sum += i;
-                if (i != (num / i)) {
-                    sum += (num / i);
+                if (i != num / i) {
+                    sum += num / i;
                 }
             }
         }
     
-        if (sum == num) {
-            isPerfect = true;
-        }
-        
-        return isPerfect;
+        return sum == num;
     }
 };
