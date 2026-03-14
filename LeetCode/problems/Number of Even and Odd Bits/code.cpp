2cba9c6fbd3fed1cb04d2ce2b5766bdbb12b41a2
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-03-13 18:15:22 -0600

    Number of Even and Odd Bits

---
+++ LeetCode/problems/Number of Even and Odd Bits/code.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> evenOddBit(int n) {
+        vector<int> res(2, 0);
+
+        for (int i = 0; n > 0; ++i) {
+            if ((n & 1) == 1) {
+                ++res[i % 2];
+            }
+        
+            n >>= 1;
+        }
+        
+        return res;      
+    }
+};
